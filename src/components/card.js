7e3ede5b8b3875d40f574b8fc68fc1f93360cb66
vit che <privsl408@gmail.com>
2024-11-20 16:58:07 +0300

    'hhhhhh'

--- src/components/card.js
+++ src/components/card.js
@@ -1,5 +1,5 @@
 // Функция создания карточки
-  export function cardData(item, deleteCard) {
+  export function createCard(cardData, deleteCard) {
   const cardTemplate = document.querySelector("#card-template").content;
   const cardElement = cardTemplate
     .querySelector(".places__item")
@@ -8,8 +8,8 @@
   const deleteButton = cardElement.querySelector(".card__delete-button");
   const cardImage = cardElement.querySelector(".card__image");
 
-  cardImage.src = item.link;
-  cardImage.alt = item.name;
+  cardImage.src = cardData.link;
+  cardImage.alt = cardData.name;
   cardElement.querySelector(".card__title").textContent = item.name;
 
   deleteButton.addEventListener("click", deleteCard);
